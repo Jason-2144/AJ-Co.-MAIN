f5aa2f0f2b3726b9f8082178e6f09b0f6b4439e6
Jason-2144 <jsnashish@gmail.com>
2026-06-09 01:38:05 +0530

    feat: add application routes for site pages
    Configure React Router to include all primary application pages, including services, case studies, company information, and legal documentation.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,15 @@ import Footer from './components/Footer';
 
 // Pages
 import Home from './pages/Home';
+import Services from './pages/Services';
+import ServiceDetail from './pages/ServiceDetail';
+import Process from './pages/Process';
+import CaseStudies from './pages/CaseStudies';
+import CaseStudyDetail from './pages/CaseStudyDetail';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import TermsOfService from './pages/TermsOfService';
 
 export default function App() {
   return (
@@ -14,7 +23,15 @@ export default function App() {
         <main className="flex-grow pt-24">
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* Additional routes will be added here */}
+            <Route path="/services" element={<Services />} />
+            <Route path="/services/:id" element={<ServiceDetail />} />
+            <Route path="/process" element={<Process />} />
+            <Route path="/case-studies" element={<CaseStudies />} />
+            <Route path="/case-studies/:id" element={<CaseStudyDetail />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-service" element={<TermsOfService />} />
           </Routes>
         </main>
         <Footer />
